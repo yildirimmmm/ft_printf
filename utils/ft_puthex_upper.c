c939f07a4a609ac835482e2802f655a00cea78f9
Muhammet Yıldırım <66938690+yildirim000@users.noreply.github.com>
2024-07-05 00:01:29 +0300

    Utils: Output uppercase hex to file descriptor

---
+++ utils/ft_puthex_upper.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex_upper.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 00:00:00 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/07/12 03:22:07 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+static int	ft_count_hex_digits(unsigned long n)
+{
+	int	count;
+
+	if (n == 0)
+		return (1);
+	count = 0;
+	while (n != 0)
+	{
+		n /= 16;
+		count++;
+	}
+	return (count);
+}
+
+int	ft_puthex_upper(unsigned long n, int fd)
+{
+	char	*hex_digits;
+	int		printed_chars;
+
+	hex_digits = "0123456789ABCDEF";
+	printed_chars = ft_count_hex_digits(n);
+	if (n >= 16)
+		ft_puthex_upper(n / 16, fd);
+	ft_putchar_fd(hex_digits[n % 16], fd);
+	return (printed_chars);
+}
